df129ad45fcc8c94824bbef1a36f01ac9d8aa9a7
abdo <elabdessamad.me@gmail.com>
2023-06-16 15:01:44 -0700

    prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 /*
  * File: 9-print_comb.c
- * Auth: Abdessamad El Fathi
+ * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
@@ -18,7 +18,7 @@ int main(void)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
-		  continue;
+			continue;		
 		putchar(',');
 		putchar(' ');
 	}
