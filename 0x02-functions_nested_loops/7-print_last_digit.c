9050e46ebb00f163b5a5e97978d947347e7caa24
abdo <elabdessamad.me@gmail.com>
2023-06-20 08:28:58 -0700

    print last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,8 +14,13 @@
 
 int print_last_digit(int n)
 {
-	n *= n < 0 ? -1 : 1;
-	n %= 10;
-	_putchar(n + '0');
-	return (n);
+	int last_digit;
+
+	last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
