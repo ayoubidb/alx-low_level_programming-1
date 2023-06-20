a6039d989532c1194d0668cd6e9de897618de53a
abdo <elabdessamad.me@gmail.com>
2023-06-20 09:10:53 -0700

    return the addiction of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/*
+ * File: 10-add.c
+ * Author: Abdessamad El fathi
+ */
+
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: the first number in the addiction
+ * @b: the second number in the addiction
+ *
+ * Return: the sum of the integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
