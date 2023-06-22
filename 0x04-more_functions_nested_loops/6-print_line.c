9bbf16840bd68e94e2f13b2a9a8d5637cf1f8d76
abdo <elabdessamad.me@gmail.com>
2023-06-22 07:09:26 -0700

    6: draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+/*
+ * File: 6-print_line.c
+ * Author: Abdessamad El Fathi
+ */
+
+#include "main.h"
+
+/**
+ * print_line -  a function that draws a straight line in the terminal.
+ * @n : the number of time _ should be printed
+ *
+ * Return : void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+
+	_putchar('\n');
+}
