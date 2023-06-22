92a5c579de6030ddd2a1c43caabeb2f340aa00bd
abdo <elabdessamad.me@gmail.com>
2023-06-22 08:21:49 -0700

    10: draw trianble

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+/*
+ * File: 10-print_triangle.c
+ * Author: Abdessamad El Fathi
+ */
+
+#include "main.h"
+
+/**
+ * print_triangle -  a function that prints a triangle, followed by a new line.
+ * @size: the size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+			_putchar(' ');
+
+		for (k = j; k < size; k++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
