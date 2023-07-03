c8cee5769a9a9723e622450ecd72c71e42ba0918
abdo <elabdessamad.me@gmail.com>
2023-07-03 13:56:12 -0700

    100: a function that sets the value of a pointer to a char.

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+ * set_string - redirect.
+ * @s: double pointer to address
+ * @to: the original pointer
+ *
+ * Return: chessboard.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
