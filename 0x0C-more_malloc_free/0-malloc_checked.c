aa4ffc07ccf97834b375aca635a6b4b6d60a8dee
abdo <elabdessamad.me@gmail.com>
2023-07-13 13:23:04 -0700

    0 : check malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,18 @@
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * malloc_checked - allocates memory using malloc
- * @b: size of the new array
+ * malloc_checked - allocates memory using malloc.
+ * @b: size of new memory array.
  *
- * Return: the string initiated
+ *
+ * Return: the string initialized.
  */
-
 void *malloc_checked(unsigned int b)
 {
-	void* memLoc;
+	void *memLoc;
 
 	memLoc = malloc(b);
-
 	if (memLoc == NULL)
 		exit(98);
-
 	return (memLoc);
 }
